44ca187ea65f1fc0d70771e9022161b84d1da194
imara abdullah <102338647+imaraabdullah0@users.noreply.github.com>
2022-11-09 22:55:05 +0200

    third

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -17,7 +17,7 @@ const MyProject = () => {
         <img alt="" src={front} class="w-full align-middle rounded-t-lg"/>
         <div class="relative p-8 mb-4">
           <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" class="absolute left-0 w-full block h-95-px -top-94-px">
-            <polygon points="-30,95 583,95 583,65" class="text-pink-500 fill-current"></polygon>
+            <polygon points="-30,95 583,95 583,65" class="text-pink-500 fill-current "></polygon>
           </svg>
           <h4 class="text-xl font-bold text-white">
             Great for your awesome project
