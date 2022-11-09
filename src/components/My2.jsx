5cf8754a54350b6e171797c0a5823aceb87e5da1
imara abdullah <102338647+imaraabdullah0@users.noreply.github.com>
2022-11-10 01:36:02 +0200

    four

--- src/components/My2.jsx
+++ src/components/My2.jsx
@@ -4,10 +4,221 @@ import {AiFillGithub,AiTwotoneNotification,AiOutlineTrademarkCircle} from 'react
 
 const My2 = () => {
   return (
-    <div className='all'>
-    
-    </div>
+    <>
+    <section class="text-gray-600 body-font overflow-hidden">
+			<div class="container py-24 mx-auto">
+				<div class="flex flex-col text-center w-full mb-20">
+					<h1
+						class="sm:text-4xl text-3xl font-bold title-font mb-2 text-gray-900"
+					>
+						Card List
+					</h1>
+
+				 <div
+						class="flex mx-auto border-2 border-indigo-500 rounded overflow-hidden mt-6"
+					>
+						<button
+							class="py-1 px-4 bg-indigo-500 text-white focus:outline-none"
+						>
+							Monthly
+						</button>
+						<button class="py-1 px-4 focus:outline-none">Annually</button>
+					</div>
+				</div>
+				<div class="flex flex-wrap justify-center">
+					<div class="p-4 xl:w-1/4 md:w-1/2 w-7/12 mb-12">
+						<div
+							class="bg-green-500 h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl"
+						>
+							<span
+								class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl"
+								>National Rank #1</span
+							>
+							<div
+								class="h-48"
+								
+							>
+								<div class="text-4xl text-white mt-4 ml-4">Tesla</div>
+							</div>
+							<div
+								class="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900"
+							>
+								<p>
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+									Sequi unde maiores nihil officiis voluptatum numquam quisquam,
+									veritatis placeat aliquid velit, saepe, quae totam cupiditate.
+									Iure quas quam dolorem magnam ut.
+								</p>
+							</div>
+						</div>
+						<button
+							class="flex items-center mt-auto text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none hover:bg-red-400 rounded-xl shadow-xl"
+						>
+							Button
+						</button>
+					</div>
+					<div class="p-4 xl:w-1/4 md:w-1/2 w-7/12 mb-12">
+						<div
+							class="bg-yellow-500 h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl"
+						>
+							<span
+								class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl"
+								>National Rank #1</span
+							>
+							<div
+								class="h-48"
+						
+							>
+								<div class="text-4xl text-white mt-4 ml-4">Tesla</div>
+							</div>
+							<div
+								class="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900"
+							>
+								<p>
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+									Sequi unde maiores nihil officiis voluptatum numquam quisquam,
+									veritatis placeat aliquid velit, saepe, quae totam cupiditate.
+									Iure quas quam dolorem magnam ut.
+								</p>
+							</div>
+						</div>
+						<button
+							class="flex items-center mt-auto text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none hover:bg-red-400 rounded-xl shadow-xl"
+						>
+							Button
+						</button>
+					</div>
+					<div class="p-4 xl:w-1/4 md:w-1/2 w-7/12 mb-12">
+						<div
+							class="bg-blue-400 h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl"
+						>
+							<span
+								class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl"
+								>National Rank #1</span
+							>
+							<div
+								class="h-48"
+							
+							>
+								<div class="text-4xl text-white mt-4 ml-4">Tesla</div>
+							</div>
+							<div
+								class="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900"
+							>
+								<p>
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+									Sequi unde maiores nihil officiis voluptatum numquam quisquam,
+									veritatis placeat aliquid velit, saepe, quae totam cupiditate.
+									Iure quas quam dolorem magnam ut.
+								</p>
+							</div>
+						</div>
+						<button
+							class="flex items-center mt-auto text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none hover:bg-red-400 rounded-xl shadow-xl"
+						>
+							Button
+						</button>
+					</div>
+					<br />
+					<div class="p-4 xl:w-1/4 md:w-1/2 w-7/12 mb-12">
+						<div
+							class="bg-green-500 h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl"
+						>
+							<span
+								class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl"
+								>National Rank #1</span
+							>
+							<div
+								class="h-48"
+								
+									
+							>
+								<div class="text-4xl text-white mt-4 ml-4">Tesla</div>
+							</div>
+							<div
+								class="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900"
+							>
+								<p>
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+									Sequi unde maiores nihil officiis voluptatum numquam quisquam,
+									veritatis placeat aliquid velit, saepe, quae totam cupiditate.
+									Iure quas quam dolorem magnam ut.
+								</p>
+							</div>
+						</div>
+						<button
+							class="flex items-center mt-auto text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none hover:bg-red-400 rounded-xl shadow-xl"
+						>
+							Button
+						</button>
+					</div>
+					<div class="p-4 xl:w-1/4 md:w-1/2 w-7/12 mb-12">
+						<div
+							class="bg-yellow-500 h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl"
+						>
+							<span
+								class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl"
+								>National Rank #1</span
+							>
+							<div
+								class="h-48"
+							
+							>
+								<div class="text-4xl text-white mt-4 ml-4">Tesla</div>
+							</div>
+							<div
+								class="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900"
+							>
+								<p>
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+									Sequi unde maiores nihil officiis voluptatum numquam quisquam,
+									veritatis placeat aliquid velit, saepe, quae totam cupiditate.
+									Iure quas quam dolorem magnam ut.
+								</p>
+							</div>
+						</div>
+						<button
+							class="flex items-center mt-auto text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none hover:bg-red-400 rounded-xl shadow-xl"
+						>
+							Button
+						</button>
+					</div>
+					<div class="p-4 xl:w-1/4 md:w-1/2 w-7/12 mb-12">
+						<div
+							class="bg-blue-400 h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl"
+						>
+							<span
+								class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl"
+								>National Rank #1</span
+							>
+							<div
+								class="h-48"
+								
+							>
+								<div class="text-4xl text-white mt-4 ml-4">Tesla</div>
+							</div>
+							<div
+								class="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900"
+							>
+								<p>
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+									Sequi unde maiores nihil officiis voluptatum numquam quisquam,
+									veritatis placeat aliquid velit, saepe, quae totam cupiditate.
+									Iure quas quam dolorem magnam ut.
+								</p>
+							</div>
+						</div>
+						<button
+							class="flex items-center mt-auto text-white bg-red-600 border-0 py-2 px-4 w-full focus:outline-none hover:bg-red-400 rounded-xl shadow-xl"
+						>
+							Button
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
+    </>
   )
-}
+};
 
 export default My2
